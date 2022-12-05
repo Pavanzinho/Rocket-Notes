6343cca6575c1ff0b511f7ffbf1b868bc5cff422
Thiago Pavan da Silva <96990538+Pavanzinho@users.noreply.github.com>
2022-12-05 10:32:57 -0300

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
+// para ver as páginas pós login, trocar <AuthRoutes> por <AppRoutes>
 
 export function Routes(){
     return(
